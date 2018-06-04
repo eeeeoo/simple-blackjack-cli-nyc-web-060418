331b39a951372e6af349996156134fcb78c69313
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-04 09:30:43 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,10 +3,9 @@ def welcome
   puts "Welcome to the Blackjack Table"
 end
 
-def deal_card(card)
+def deal_card
   # code #deal_card here
-  card = 1..11
-  card > 0
+  rand(1..11)
 end
 
 def display_card_total
